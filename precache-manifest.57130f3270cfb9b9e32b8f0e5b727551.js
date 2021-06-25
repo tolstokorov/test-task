c72178b3c563c3e205f57f07126af31ae6222d4e
Sergey Tolstokorov <stolstokorov2020@yandex.ru>
2021-06-25 10:42:37 +0700

    Updates

---
+++ precache-manifest.57130f3270cfb9b9e32b8f0e5b727551.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870b5d9eb44755f39902735b2478c551",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "5b361f65b88d4a43c3fb",
+    "url": "/test-task/static/css/0.8b2aadfe.chunk.css"
+  },
+  {
+    "revision": "a6d67768c5e6c075636b",
+    "url": "/test-task/static/css/main.1b12d644.chunk.css"
+  },
+  {
+    "revision": "5b361f65b88d4a43c3fb",
+    "url": "/test-task/static/js/0.7a95a34b.chunk.js"
+  },
+  {
+    "revision": "6a158f341dc1e3049568872cfa992e48",
+    "url": "/test-task/static/js/0.7a95a34b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb40501b3c9d2acddaa",
+    "url": "/test-task/static/js/10.bb983cd1.chunk.js"
+  },
+  {
+    "revision": "3524b589bd922cdecd28",
+    "url": "/test-task/static/js/11.c66ec230.chunk.js"
+  },
+  {
+    "revision": "bc287e85d87f12a918e8",
+    "url": "/test-task/static/js/3.9b444e56.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-task/static/js/3.9b444e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e2845df4abc8e01e6d",
+    "url": "/test-task/static/js/4.5434bd1e.chunk.js"
+  },
+  {
+    "revision": "1837935e9f5dd8a5295d",
+    "url": "/test-task/static/js/5.afb99e48.chunk.js"
+  },
+  {
+    "revision": "5e2f630e5dd0913793de",
+    "url": "/test-task/static/js/6.631fa14a.chunk.js"
+  },
+  {
+    "revision": "27a95714d669cdfaa93b",
+    "url": "/test-task/static/js/7.cdbaf786.chunk.js"
+  },
+  {
+    "revision": "05cf3be38df67d99c8d7",
+    "url": "/test-task/static/js/8.f82d965c.chunk.js"
+  },
+  {
+    "revision": "f450d24956f10e908619",
+    "url": "/test-task/static/js/9.d681b1fb.chunk.js"
+  },
+  {
+    "revision": "a6d67768c5e6c075636b",
+    "url": "/test-task/static/js/main.4e178291.chunk.js"
+  },
+  {
+    "revision": "225e931e1db8da284d12",
+    "url": "/test-task/static/js/runtime-main.a8676618.js"
+  }
+]);
